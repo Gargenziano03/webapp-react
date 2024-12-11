d146f38d9cf7f99affc888d245ec40f010942962
Gargenziano03 <gargenziano03@gmail.com>
2024-12-11 16:06:13 +0100

    add AppLayout + route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AppLayout from "./layouts/AppLayout"
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
