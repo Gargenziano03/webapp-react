179724c574fb92322b7cd9dd12ee7c0328e6b0a0
Gargenziano03 <gargenziano03@gmail.com>
2024-12-12 02:42:01 +0100

    Add notfound

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AppLayout from "./layouts/AppLayout"
 import MoviesPage from "./pages/MoviesPage"
 import SingleMoviePage from "./pages/SingleMoviePage"
+import NotFound from "./components/NotFound"
+
 function App() {
 
   return (
@@ -11,6 +13,7 @@ function App() {
           <Route element={<AppLayout />}>
             <Route index element={<MoviesPage />} />
             <Route path="movies/:id" element={<SingleMoviePage />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
