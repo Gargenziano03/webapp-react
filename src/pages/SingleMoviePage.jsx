fb55a74a7ae330489539b8cacbdf8f30d9150bc9
Gargenziano03 <gargenziano03@gmail.com>
2024-12-13 16:39:05 +0100

    add api review

--- src/pages/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage.jsx
@@ -1,44 +1,59 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Banner from "../components/Banner";
 import ReviewCard from "../components/ReviewCard";
 useParams
 export default function SingleMoviePage() {
     const { id } = useParams()
+    const base_movie_api_url = `http://localhost:3000/api/movies/${id}`
+    const [movie, setMovie] = useState(null)
 
-    const reviews = [
-        {
-            id: 1,
-            name: 'Alice',
-            vote: 5,
-            text: 'A mind-bending masterpiece.',
-            created_at: '2024-11-29 11:40:13',
-            updated_at: '2024-11-29 11:40:13'
-        },
-        {
-            id: 2,
-            name: 'Bob',
-            vote: 4,
-            text: 'Great visuals and a compelling story.',
-            created_at: '2024-11-29 11:40:13',
-            updated_at: '2024-11-29 11:40:13'
-        },
-        {
-            id: 3,
-            name: 'Charlie',
-            vote: 3,
-            text: 'Confusing at times, but worth watching.',
-            created_at: '2024-11-29 11:40:13',
-            updated_at: '2024-11-29 11:40:13'
-        },
-    ]
+    useEffect(() => {
+        fetch(base_movie_api_url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+
+                setMovie(data)
+                console.log(movie);
+
+
+            }).catch(err => console.error(err))
+    }, [])
+    /* const reviews = [
+         {
+             id: 1,
+             name: 'Alice',
+             vote: 5,
+             text: 'A mind-bending masterpiece.',
+             created_at: '2024-11-29 11:40:13',
+             updated_at: '2024-11-29 11:40:13'
+         },
+         {
+             id: 2,
+             name: 'Bob',
+             vote: 4,
+             text: 'Great visuals and a compelling story.',
+             created_at: '2024-11-29 11:40:13',
+             updated_at: '2024-11-29 11:40:13'
+         },
+         {
+             id: 3,
+             name: 'Charlie',
+             vote: 3,
+             text: 'Confusing at times, but worth watching.',
+             created_at: '2024-11-29 11:40:13',
+             updated_at: '2024-11-29 11:40:13'
+         },
+ ]*/
 
     return (
         <>
-            <Banner title="movie" subtitle="common films" />
+            <Banner title={movie?.title} subtitle={movie?.director} />
 
             <section className="reviews">
                 <div className="container">
-                    {reviews.map((review) => <ReviewCard key={review.id} review={review} />)}
+                    {movie && movie?.reviews.map((review) => <ReviewCard key={review.id} review={review} />)}
                 </div>
             </section>
         </>
