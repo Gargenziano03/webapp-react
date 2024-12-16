839edb60b309cb5bf337d7c9a82c4d3ee124232d
Gargenziano03 <gargenziano03@gmail.com>
2024-12-16 15:09:55 +0100

    add fix

--- src/components/ReviewFormCard.jsx
+++ src/components/ReviewFormCard.jsx
@@ -1,46 +1,52 @@
 import { useState } from "react"
-export default function textFormCard() {
+export default function textFormCard({ movie_id }) {
 
     const [name, setName] = useState('')
     const [text, setText] = useState('')
     const [rating, setRating] = useState(0)
-
+    const [errorMessage, setErrorMessage] = useState(null)
 
     function HandleFormSubmit(e) {
         e.preventDefault()
 
-        const formData = {
-            name,
-            text,
-            vote: rating
-        }
-        console.log(formData);
+        if (name.length < 2 || text.length < 5 || rating == 0) {
+            setErrorMessage('Please fill all fileds in the form')
 
-
-        const base_movie_api_url = `http://localhost:3000/api/movies/${movie_id}/reviews`
-        fetch(base_movie_api_url, {
-            method: 'POST',
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-Type": "application/json"
+        } else {
+            setErrorMessage(null)
+            const formData = {
+                name,
+                text,
+                vote: rating
             }
-        }).then(res => res.json())
-            .then(data => {
-                console.log(data);
+            console.log(formData);
 
-            }).catch(err => console.log(err));
 
+            const base_movie_api_url = `http://localhost:3000/api/movies/${movie_id}/review`
+            fetch(base_movie_api_url, {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(err => console.log(err
+
+                ));
+        }
     }
-    function HandleFormSubmit(e) {
-        e.preventDefault()
-    }
+
     return (
         <div className="container">
             <div className="card">
                 <div className="card-body">
                     <form onSubmit={HandleFormSubmit}>
                         <div className="mb-3">
-                            <input name="name" type="text" className="form-control" placeholder="name" value={name} onChange={(e) => setName(e.target.name)} />
+                            <input name="name" type="text" className="form-control" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
                         </div>
 
                         <div className="rating mb-3 text-warning">
@@ -50,7 +56,12 @@ export default function textFormCard() {
                         <div className="mb-3">
                             <textarea name="text" id="text" className="form-control" placeholder="text" value={text} onChange={(e) => setText(e.target.value)}></textarea>
                         </div>
-                        <button type="submit" className="btn btn-primary">invio</button>
+
+                        <div className="mb-3">
+                            <button type="submit" className="btn btn-primary">invio</button>
+                            {errorMessage && <span className="text-danger"> <i className="bi bi-x"></i> {errorMessage}</span>}
+                        </div>
+
                     </form>
                 </div>
             </div>
