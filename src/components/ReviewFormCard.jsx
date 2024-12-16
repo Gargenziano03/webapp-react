35cd4f8335a106442981617759b783a6dee3be52
Gargenziano03 <gargenziano03@gmail.com>
2024-12-16 07:54:44 +0100

    add function HandleFormSubmit

--- src/components/ReviewFormCard.jsx
+++ src/components/ReviewFormCard.jsx
@@ -1,8 +1,15 @@
 export default function ReviewFormCard() {
+    function HandleFormSubmit(e) {
+        e.preventDefault()
+    }
     return (
         <div className="container">
             <div className="card">
-                <div className="card-body"></div>
+                <div className="card-body">
+                    <form onSubmit={HandleFormSubmit}>
+
+                    </form>
+                </div>
             </div>
         </div>
     )
