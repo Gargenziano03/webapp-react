c56d308b366f01caf6f53d9f37891d12a2f37d00
Gargenziano03 <gargenziano03@gmail.com>
2024-12-16 08:03:31 +0100

    add inport username

--- src/components/ReviewFormCard.jsx
+++ src/components/ReviewFormCard.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
 export default function ReviewFormCard() {
+
+    const [username, setUsername] = useState('')
+    const [review, setReview] = useState('')
+    const [rating, setRating] = useState(0)
     function HandleFormSubmit(e) {
         e.preventDefault()
     }
@@ -7,7 +12,7 @@ export default function ReviewFormCard() {
             <div className="card">
                 <div className="card-body">
                     <form onSubmit={HandleFormSubmit}>
-
+                        <inprt name="username" type="text" className="form-control" value={username} onChange={(e) => setUsername(e.target.username)} />
                     </form>
                 </div>
             </div>
